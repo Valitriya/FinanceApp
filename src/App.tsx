e0e66498b5d46137a7358627803aa12b0eeafe54
Valentina <Vallitriya@gmail.com>
2023-10-11 20:26:42 +0300

    implemented the main app component using a theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-      <div className='app'>    
-      </div>
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 
-  )
+function App() {
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem"></Box>
+				</ThemeProvider>
+		</div>
+	);
 }
 
-export default App
+export default App;
