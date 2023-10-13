e8ee8dd5d388130e301663a9a94f052bfbe92886
Valentina <Vallitriya@gmail.com>
2023-10-13 21:22:16 +0300

    add hover effect and dashboard link in navigation

--- src/tables/navbar/index.tsx
+++ src/tables/navbar/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useTheme } from "@mui/material";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 import FlexBetween from "@/components/FlexBetween";
 import PixIcon from "@mui/icons-material/Pix";
 
 const Navbar = () => {
 	const { palette } = useTheme();
-	const  [selected, setSelected] = useState("dashboard");
+	const [selected, setSelected] = useState("dashboard");
 	return (
 		<FlexBetween mb="0.28rem" p="0.5rem 0rem" color={palette.grey[300]}>
 			<FlexBetween gap="0.72rem">
@@ -14,6 +15,20 @@ const Navbar = () => {
 				<Typography variant="h4" fontSize="18px">
 					Finanseer
 				</Typography>
+			</FlexBetween>
+			<FlexBetween gap="2rem">
+				<Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+					<Link
+						to="/"
+						onClick={() => setSelected("dashboard")}
+						style={{
+							color: selected === "dashboard" ? "inherit" : palette.grey[700],
+							textDecoration: "inherit"
+						}}
+					>
+						DashBoard
+					</Link>
+				</Box>
 			</FlexBetween>
 		</FlexBetween>
 	);
