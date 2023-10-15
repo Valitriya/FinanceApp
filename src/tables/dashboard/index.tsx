6a166da1d8bb2d554e82a76a1cde21142b4d64ea
Valentina <Vallitriya@gmail.com>
2023-10-16 00:03:41 +0300

    added dashboardbox components using styles in dashboard

--- src/tables/dashboard/index.tsx
+++ src/tables/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Box, useMediaQuery } from "@mui/material";
+import DashboardBox from "@/components/DashboardBox";
 
 const gridTemplateLargeScreens = `
 "a b c"
@@ -66,16 +67,16 @@ const Dashboard = () => {
 					  }
 			}
 		>
-			<Box bgcolor="#fff" gridArea="a"></Box>
-			<Box bgcolor="#fff" gridArea="b"></Box>
-			<Box bgcolor="#fff" gridArea="c"></Box>
-			<Box bgcolor="#fff" gridArea="d"></Box>
-			<Box bgcolor="#fff" gridArea="e"></Box>
-			<Box bgcolor="#fff" gridArea="f"></Box>
-			<Box bgcolor="#fff" gridArea="h"></Box>
-			<Box bgcolor="#fff" gridArea="g"></Box>
-			<Box bgcolor="#fff" gridArea="i"></Box>
-			<Box bgcolor="#fff" gridArea="j"></Box>
+			<DashboardBox gridArea="a"></DashboardBox>
+			<DashboardBox gridArea="b"></DashboardBox>
+			<DashboardBox gridArea="c"></DashboardBox>
+			<DashboardBox gridArea="d"></DashboardBox>
+			<DashboardBox gridArea="e"></DashboardBox>
+			<DashboardBox gridArea="f"></DashboardBox>
+			<DashboardBox gridArea="h"></DashboardBox>
+			<DashboardBox gridArea="g"></DashboardBox>
+			<DashboardBox gridArea="i"></DashboardBox>
+			<DashboardBox gridArea="j"></DashboardBox>
 		</Box>
 	);
 };
