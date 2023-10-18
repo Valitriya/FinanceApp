98ec97a5d9e709030b051471c3a9e5a62726737d
Valentina <Vallitriya@gmail.com>
2023-10-18 22:39:36 +0300

    chore: setting up the redux store and rendering the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,8 +6,14 @@ import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
 
+export const store = configureStore({
+	reducer: { [api.reducerPath]: api.reducer},
+	middleware: (getDefault) => getDefault().concat(api.middleware),
+})
+setupListeners(store.dispatch)
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>
+	</Provider>
 );
