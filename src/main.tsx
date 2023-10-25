ab2f83ce120ea40f7ae3930d2296b1b319c5f1cc
Valentina <Vallitriya@gmail.com>
2023-10-25 18:57:46 +0300

    fix path in configurestore import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
+import { configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
 
