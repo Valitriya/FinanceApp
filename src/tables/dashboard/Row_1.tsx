91f6609ee68ac02a65e8f32bb0ccdce072a0ec6c
Valentina <Vallitriya@gmail.com>
2023-10-27 21:21:44 +0300

    colors are converted to constants for better readability and color changeability

--- src/tables/dashboard/Row_1.tsx
+++ src/tables/dashboard/Row_1.tsx
@@ -16,6 +16,18 @@ const Row_1 = () => {
 	const { palette } = useTheme();
 	const { data } = useGetKpisQuery();
 
+	const MONTHLY_DATA_DOMAIN = [8000, 23000];
+
+	const COLORS_STROKE = {
+		PRIMARY: palette.primary.main,
+		SECONDARY: palette.secondary.dark,
+	};
+
+	const REVENUE_COLOR_TOP = palette.primary[500];
+	const EXPENSES_COLOR_TOP = palette.secondary[500];
+	const REVENUE_COLOR_DOWN = palette.primary[400];
+	const EXPENSES_COLOR_DOWN = palette.secondary[300];
+
 	const revenueExpenses = useMemo(() => {
 		if (!data || !Array.isArray(data[0]?.monthlyData)) return [];
 
@@ -49,31 +61,31 @@ const Row_1 = () => {
 							tickLine={false}
 							axisLine={{ strokeWidth: "0" }}
 							style={{ fontSize: "10px" }}
-							domain={[8000, 23000]}
+							domain={MONTHLY_DATA_DOMAIN}
 						/>
 						<Tooltip />
 						<defs>
 							<linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
 								<stop
 									offset="5%"
-									stopColor={palette.primary[500]}
+									stopColor={REVENUE_COLOR_TOP}
 									stopOpacity={0.3}
 								/>
 								<stop
 									offset="95%"
-									stopColor={palette.primary[300]}
+									stopColor={REVENUE_COLOR_DOWN}
 									stopOpacity={0}
 								/>
 							</linearGradient>
 							<linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
 								<stop
 									offset="5%"
-									stopColor={palette.secondary[400]}
+									stopColor={EXPENSES_COLOR_DOWN}
 									stopOpacity={0.2}
 								/>
 								<stop
 									offset="95%"
-									stopColor={palette.secondary[500]}
+									stopColor={EXPENSES_COLOR_TOP}
 									stopOpacity={0.8}
 								/>
 							</linearGradient>
@@ -82,7 +94,7 @@ const Row_1 = () => {
 							type="monotone"
 							dataKey="revenue"
 							dot={true}
-							stroke={palette.primary.main}
+							stroke={COLORS_STROKE.PRIMARY}
 							fillOpacity={1}
 							fill="url(#colorRevenue)"
 						/>
@@ -90,7 +102,7 @@ const Row_1 = () => {
 							type="monotone"
 							dataKey="expenses"
 							dot={true}
-							stroke={palette.secondary.dark}
+							stroke={COLORS_STROKE.SECONDARY}
 							fillOpacity={1}
 							fill="url(#colorExpenses)"
 						/>
