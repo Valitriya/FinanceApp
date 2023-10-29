e5a219c929485eea3154f29a6b2186d7088e519b
Valentina <Vallitriya@gmail.com>
2023-10-29 23:35:53 +0300

    added sideText

--- src/components/BoxHeader.tsx
+++ src/components/BoxHeader.tsx
@@ -2,11 +2,12 @@ import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "./FlexBetween";
 type Props = {
 	title: string;
-  subtitle?: string;
+	subtitle?: string;
+	sideText: string;
 	icon?: React.ReactNode;
 };
 
-const BoxHeader = ({ title, icon, subtitle }) => {
+const BoxHeader = ({ title, icon, subtitle, sideText }: Props) => {
 	const { palette } = useTheme();
 	return (
 		<FlexBetween color={palette.grey[400]} margin="1.5rem 1rem 0 1rem">
@@ -16,9 +17,12 @@ const BoxHeader = ({ title, icon, subtitle }) => {
 					<Typography variant="h4" mb="-0.1rem">
 						{title}
 					</Typography>
-          <Typography variant="h6">{subtitle}</Typography>
+					<Typography variant="h6">{subtitle}</Typography>
 				</Box>
 			</FlexBetween>
+			<Typography variant="h5" fontWeight="700" color={palette.secondary[500]}>
+				{sideText}
+			</Typography>
 		</FlexBetween>
 	);
 };
