c91e1ae3bfc44016f1983db25c589e054289d9fe
Valentina <Vallitriya@gmail.com>
2023-11-29 16:11:51 +0300

    added graphics GridArea="d"

--- src/tables/dashboard/Row_2.tsx
+++ src/tables/dashboard/Row_2.tsx
@@ -1,12 +1,86 @@
 import DashboardBox from "@/components/DashboardBox";
+import BoxHeader from "@/components/BoxHeader";
+import { useTheme } from "@mui/material";
 import { useGetProductsQuery } from "@/state/api";
+import {
+	ResponsiveContainer,
+	CartesianGrid,
+	XAxis,
+	YAxis,
+	Tooltip,
+	LineChart,
+	Legend,
+	Line,
+} from "recharts";
 
 const Row_2 = () => {
+	const {palette} = useTheme();
+	const COLOR_FONT = palette.grey[800];
 	const {data} = useGetProductsQuery();
 	console.log("data:", data)
 	return (
 		<>
-			<DashboardBox gridArea="d"></DashboardBox>
+			<DashboardBox gridArea="d">
+				<BoxHeader
+					title="Profit and Revenue"
+					subtitle="top line represents revenue, bottom line represents expenses"
+					sideText="+4%"
+				/>
+				<ResponsiveContainer width="100%" height="100%">
+					<LineChart
+						data={revenueProfit}
+						margin={{
+							top: 20,
+							right: 5,
+							left: -5,
+							bottom: 60,
+						}}
+					>
+						<CartesianGrid
+							vertical={false}
+							stroke={COLOR_FONT}
+							strokeDasharray="3 3"
+						/>
+						<XAxis
+							dataKey="name"
+							tickLine={false}
+							style={{ fontSize: "10px", fill: COLOR_FONT }}
+						/>
+						<YAxis
+							yAxisId="left"
+							tickLine={false}
+							axisLine={false}
+							style={{ fontSize: "10px", fill: COLOR_FONT }}
+						/>
+						<YAxis
+							yAxisId="right"
+							orientation="right"
+							tickLine={false}
+							axisLine={false}
+							style={{ fontSize: "10px", fill: COLOR_FONT }}
+						/>
+						<Tooltip />
+						<Legend
+							height={20}
+							wrapperStyle={{
+								margin: "0 0 10px 0",
+							}}
+						/>
+						<Line
+							yAxisId="left"
+							type="monotone"
+							dataKey="Non Operational Expenses"
+							stroke={palette.tertiary[500]}
+						/>
+						<Line
+							yAxisId="right"
+							type="monotone"
+							dataKey="Operational Expenses"
+							stroke={palette.primary[500]}
+						/>
+					</LineChart>
+				</ResponsiveContainer>
+			</DashboardBox>
 			<DashboardBox gridArea="e"></DashboardBox>
 			<DashboardBox gridArea="f"></DashboardBox>
 		</>
