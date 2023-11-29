1b45261cea39bf1fa1450dec9bea358fa9b185ae
Valentina <Vallitriya@gmail.com>
2023-11-29 16:37:42 +0300

    change interface MonthlyDataItem

--- src/state/types.ts
+++ src/state/types.ts
@@ -1,47 +1,49 @@
 export interface ExpansesByCategory {
-    salaries: number;
-    supplies: number;
-    services: number;
+	salaries: number;
+	supplies: number;
+	services: number;
 }
-export interface Month{
-    id: string;
-    month: string;
-    revenue: number;
-    expenses: number;
-    nonOperationalExpenses: number;
-    operationalExpenses: number;
+export interface Month {
+	id: string;
+	month: string;
+	revenue: number;
+	expenses: number;
+	nonOperationalExpenses: number;
+	operationalExpenses: number;
 }
-export interface Day{
-    id: string;
-    date: string;
-    revenue: number;
-    expenses: number;
+export interface Day {
+	id: string;
+	date: string;
+	revenue: number;
+	expenses: number;
 }
-export interface MonthlyDataItem{
-    revenue: number,
-    expenses: number,
-    profit?: number;
+export interface MonthlyDataItem {
+	revenue?: number;
+	expenses?: number;
+	profit?: number;
+	"Operational Expenses"?: number;
+	"Non Operational Expenses"?: number;
 }
-export interface GetKpisResponse{
-    id: string;
-    __id: string;
-    __v: number;
-    totalProfit: number;
-    totalRevenue: number;
-    totalExpenses: number;
-    expensesByCategory: ExpansesByCategory;
-    monthlyData: Array<Month>;
-    dailyData: Array<Day>;
-    createdAt: string;
-    updatedAt: string;  
+export interface GetKpisResponse {
+	id: string;
+	__id: string;
+	__v: number;
+	totalProfit: number;
+	totalRevenue: number;
+	totalExpenses: number;
+	expensesByCategory: ExpansesByCategory;
+	monthlyData: Array<Month>;
+	dailyData: Array<Day>;
+	createdAt: string;
+	updatedAt: string;
 }
-export interface GetProductsResponse{
-    id: string;
-    __id: string;
-    __v: number;
-    price: number;
-    expense: number;
-    transactions: Array<string>;
-    createdAt: string;
-    updatedAt: string;   
+export interface GetProductsResponse {
+	id: string;
+	__id: string;
+	__v: number;
+	price: number;
+	expense: number;
+	transactions: Array<string>;
+	createdAt: string;
+	updatedAt: string;
 }
