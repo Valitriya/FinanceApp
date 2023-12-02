b2db996203936ee94fc0913aa69a0da6bc8f586e
Valentina <Vallitriya@gmail.com>
2023-12-02 22:33:30 +0300

    added Typography with number indicator

--- src/tables/dashboard/Row_2.tsx
+++ src/tables/dashboard/Row_2.tsx
@@ -134,6 +134,8 @@ const Row_2 = () => {
 					</PieChart>
 					<Box ml="-0.7rem" flexBasis="40%" textAlign="center">
 						<Typography variant="h5">Target Sales</Typography>
+						<Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>83</Typography>
+					
 					</Box>
 				</FlexBetween>
 			</DashboardBox>
