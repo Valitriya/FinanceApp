824907770843fb5dd73524ef561790d698cb94f0
Valentina <Vallitriya@gmail.com>
2023-12-04 16:12:11 +0300

    added properties to XAxis

--- src/tables/dashboard/Row_2.tsx
+++ src/tables/dashboard/Row_2.tsx
@@ -166,7 +166,14 @@ const Row_2 = () => {
 						}}
 					>
 						<CartesianGrid stroke={COLOR_FONT}/>
-						<XAxis type="number" dataKey="x" name="stature" unit="cm" />
+						<XAxis 
+							type="number" 
+							dataKey="price" 
+							name="price" 
+							axisLine={false}
+							tickLine={false}
+							style={{ fontSize: "10px"}}
+							tickFormatter={(v) => `$${v}`} />
 						<YAxis type="number" dataKey="y" name="weight" unit="kg" />
 						<Tooltip cursor={{ strokeDasharray: "3 3" }} />
 						<Scatter name="A school" data={data} fill="#8884d8" />
