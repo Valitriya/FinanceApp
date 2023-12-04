1915f35d8e57da8248cc3e3760fac5d5cd3d3bcc
Valentina <Vallitriya@gmail.com>
2023-12-04 16:03:55 +0300

    added ScatterChart template

--- src/tables/dashboard/Row_2.tsx
+++ src/tables/dashboard/Row_2.tsx
@@ -152,7 +152,25 @@ const Row_2 = () => {
 					</Box>
 				</FlexBetween>
 			</DashboardBox>
-			<DashboardBox gridArea="f"></DashboardBox>
+			<DashboardBox gridArea="f">
+				<ResponsiveContainer width="100%" height={400}>
+					<ScatterChart
+						margin={{
+							top: 20,
+							right: 20,
+							bottom: 20,
+							left: 20,
+						}}
+					>
+						<CartesianGrid />
+						<XAxis type="number" dataKey="x" name="stature" unit="cm" />
+						<YAxis type="number" dataKey="y" name="weight" unit="kg" />
+						<Tooltip cursor={{ strokeDasharray: "3 3" }} />
+						<Scatter name="A school" data={data} fill="#8884d8" />
+					</ScatterChart>
+				</ResponsiveContainer>
+				);
+			</DashboardBox>
 		</>
 	);
 };
