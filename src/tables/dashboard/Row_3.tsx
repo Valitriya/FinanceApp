75525b8630cca7fcec6d53249f36f796f6487bd4
Valentina <Vallitriya@gmail.com>
2023-12-07 17:54:12 +0300

    added BoxHeader

--- src/tables/dashboard/Row_3.tsx
+++ src/tables/dashboard/Row_3.tsx
@@ -1,14 +1,24 @@
 import DashboardBox from "@/components/DashboardBox";
-import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from "@/state/api";
+import BoxHeader from "@/components/BoxHeader";
+import {
+	useGetKpisQuery,
+	useGetProductsQuery,
+	useGetTransactionsQuery,
+} from "@/state/api";
 
 const Row_3 = () => {
-	const {data: kpiData} = useGetKpisQuery();
-	const {data: productData} = useGetProductsQuery();
-	const {data: transactionData} = useGetTransactionsQuery();
+	const { data: kpiData } = useGetKpisQuery();
+	const { data: productData } = useGetProductsQuery();
+	const { data: transactionData } = useGetTransactionsQuery();
 
 	return (
 		<>
-			<DashboardBox gridArea="h"></DashboardBox>
+			<DashboardBox gridArea="h">
+				<BoxHeader
+					title="List of Products"
+					sideText={`${productData?.length} products`}
+				/>
+			</DashboardBox>
 			<DashboardBox gridArea="g"></DashboardBox>
 			<DashboardBox gridArea="i"></DashboardBox>
 			<DashboardBox gridArea="j"></DashboardBox>
