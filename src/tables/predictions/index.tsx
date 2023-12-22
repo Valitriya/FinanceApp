64ede7e94559acc80ff5a59e8efe02e5acdad09c
Valentina <Vallitriya@gmail.com>
2023-12-22 17:21:24 +0300

    constants added and imported

--- src/tables/predictions/index.tsx
+++ src/tables/predictions/index.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { useTheme } from "@emotion/react";
+import { useGetKpisQuery } from "@/state/api";
 
 const Predictions = () => {
+    const {palette} = useTheme();
+    const [isPredictions, setIsPredictions] = useState(false);
+    const { data: kpiData} = useGetKpisQuery();
   return (
     <div>Predictions</div>
   )
