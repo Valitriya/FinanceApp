d18e7c0db34f7ab9726a2e74cbf9b36338fcdffc
Valentina <Vallitriya@gmail.com>
2023-12-23 23:55:17 +0300

    added style

--- src/tables/predictions/index.tsx
+++ src/tables/predictions/index.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { useTheme } from "@emotion/react";
 import { useGetKpisQuery } from "@/state/api";
+import DashboardBox from "@/components/DashboardBox";
 
 const Predictions = () => {
-    const {palette} = useTheme();
-    const [isPredictions, setIsPredictions] = useState(false);
-    const { data: kpiData} = useGetKpisQuery();
-  return (
-    <div>Predictions</div>
-  )
-}
+	const { palette } = useTheme();
+	const [isPredictions, setIsPredictions] = useState(false);
+	const { data: kpiData } = useGetKpisQuery();
+	return (
+		<DashboardBox
+			width="100%"
+			height="100%"
+			p="1rem"
+			overflow="hidden"
+		></DashboardBox>
+	);
+};
 
 export default Predictions;
