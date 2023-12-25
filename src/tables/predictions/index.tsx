e473e9c3da679060df217c72a3c9da8a2a0567a4
Valentina <Vallitriya@gmail.com>
2023-12-25 21:28:59 +0300

    added chart template

--- src/tables/predictions/index.tsx
+++ src/tables/predictions/index.tsx
@@ -3,11 +3,23 @@ import { useGetKpisQuery } from "@/state/api";
 import { Box, Button, Typography, useTheme } from "@mui/material";
 import DashboardBox from "@/components/DashboardBox";
 import FlexBetween from "@/components/FlexBetween";
+import {
+	ResponsiveContainer,
+	CartesianGrid,
+	XAxis,
+	YAxis,
+	Tooltip,
+	LineChart,
+	Legend,
+	Line,
+} from "recharts";
 
 const Predictions = () => {
 	const { palette } = useTheme();
 	const [isPredictions, setIsPredictions] = useState(false);
 	const { data: kpiData } = useGetKpisQuery();
+	
+	const COLOR_FONT = palette.grey[800];
 	return (
 		<DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
 			<FlexBetween m="1rem 2.5rem" gap="0.3rem">
@@ -29,6 +41,59 @@ const Predictions = () => {
 					Show Predicted Revenue for Next Year
 				</Button>
 			</FlexBetween>
+			<ResponsiveContainer width="100%" height="100%">
+				<LineChart
+					data={}
+					margin={{
+						top: 20,
+						right: 5,
+						left: -5,
+						bottom: 60,
+					}}
+				>
+					<CartesianGrid
+						stroke={COLOR_FONT}
+						strokeDasharray="3 3"
+					/>
+					<XAxis
+						dataKey="name"
+						tickLine={false}
+						style={{ fontSize: "10px", fill: COLOR_FONT }}
+					/>
+					<YAxis
+						yAxisId="left"
+						tickLine={false}
+						axisLine={false}
+						style={{ fontSize: "10px", fill: COLOR_FONT }}
+					/>
+					<YAxis
+						yAxisId="right"
+						orientation="right"
+						tickLine={false}
+						axisLine={false}
+						style={{ fontSize: "10px", fill: COLOR_FONT }}
+					/>
+					<Tooltip />
+					<Legend
+						height={20}
+						wrapperStyle={{
+							margin: "0 0 10px 0",
+						}}
+					/>
+					<Line
+						yAxisId="left"
+						type="monotone"
+						dataKey="profit"
+						stroke={palette.tertiary[500]}
+					/>
+					<Line
+						yAxisId="right"
+						type="monotone"
+						dataKey="revenue"
+						stroke={palette.primary[500]}
+					/>
+				</LineChart>
+			</ResponsiveContainer>
 		</DashboardBox>
 	);
 };
