ab07e8157fbbefd7146a1f4767ba96ab8183dbfe
Valentina <Vallitriya@gmail.com>
2023-12-25 21:01:23 +0300

    FlexBetween added

--- src/tables/predictions/index.tsx
+++ src/tables/predictions/index.tsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 import { useTheme } from "@emotion/react";
 import { useGetKpisQuery } from "@/state/api";
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 
 const Predictions = () => {
 	const { palette } = useTheme();
 	const [isPredictions, setIsPredictions] = useState(false);
 	const { data: kpiData } = useGetKpisQuery();
 	return (
-		<DashboardBox
-			width="100%"
-			height="100%"
-			p="1rem"
-			overflow="hidden"
-		></DashboardBox>
+		<DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+			<FlexBetween m="1rem 2.5rem"></FlexBetween>
+		</DashboardBox>
 	);
 };
 
