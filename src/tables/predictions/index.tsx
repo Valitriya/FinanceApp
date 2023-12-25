4dae0eb8b08fc970a9a197646282801a6e645f93
Valentina <Vallitriya@gmail.com>
2023-12-25 21:14:40 +0300

    added Typography with description

--- src/tables/predictions/index.tsx
+++ src/tables/predictions/index.tsx
@@ -13,6 +13,9 @@ const Predictions = () => {
 			<FlexBetween m="1rem 2.5rem">
 				<Box>
 					<Typography variant="h3">Revenue and Predictions</Typography>
+					<Typography variant="h6">
+						charted revenue and predicted revenue based on a simple linear regression model
+					</Typography>
 				</Box>
 			</FlexBetween>
 		</DashboardBox>
